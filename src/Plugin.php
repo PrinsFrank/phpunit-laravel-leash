4b7ef779188b40f1a8bd543ed0dad593d82e20fe
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2022-12-19 10:55:05 +0100

    Add Plugin files

---
+++ src/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace PrinsFrank\PHPUnitLaravelLeash;
+
+use PHPUnit\Runner\BeforeFirstTestHook;
+use PHPUnit\Runner\BeforeTestHook;
+use PrinsFrank\LaravelLeash\LaravelLeash;
+
+class Plugin implements BeforeTestHook, BeforeFirstTestHook
+{
+    private string $enableForTestPattern;
+
+    public function __construct(string $enableForTestPattern = '/\\\\Unit\\\\/')
+    {
+        $this->enableForTestPattern = $enableForTestPattern;
+    }
+
+    public function executeBeforeFirstTest(): void
+    {
+        LaravelLeash::bootstrap();
+    }
+
+    public function executeBeforeTest(string $test): void
+    {
+        if (preg_match($this->enableForTestPattern, $test) !== 1) {
+            LaravelLeash::unLeash();
+
+            return;
+        }
+
+        LaravelLeash::leash();
+    }
+}
